8e3e4d4e0bd8db3b8fd5afa89ffccce8ef087c8e
user_name <email_id>
2023-09-28 08:46:26 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // code your solution here
+// index.js
+const record = [
+    { year: "2015", result: "W" },
+    { year: "2014", result: "N/A" },
+    { year: "2013", result: "L" },
+    // ...
+  ];
+  
+  function superbowlWin(record) {
+    // Use the find() method to search for a record with a "W" (win) result
+    const winningRecord = record.find(entry => entry.result === "W");
+  
+    // Check if a winning record was found, and return the year if found, or undefined if not
+    if (winningRecord) {
+      return winningRecord.year;
+    } else {
+      return undefined;
+    }
+  }
+  
+  // Call the superbowlWin function to find the year of the Superbowl win
+  const superBowlYear = superbowlWin(record);
+  
+  // Log the result to the console
+  console.log(`The Denver Broncos won the Superbowl in the year ${superBowlYear}`);
+  
